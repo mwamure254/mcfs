a41b35b48fae6a4c7235ffb67b23e7329940652a
mwamure254 <mwamuredkwicha@gmail.com>
2026-08-16 16:55:48 +0300

    mcfs

---
+++ src/main/java/com/mfano/mcfs/McfsApplication.java
@@ -0,0 +1,13 @@
+package com.mfano.mcfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McfsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McfsApplication.class, args);
+	}
+
+}
